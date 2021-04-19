79a50726361e76ba8bf912e25ad4257f0ae9fb3c
karthikmasi <ramadak@DESKTOP-MK1MSLU.localdomain>
2021-04-19 13:13:29 -0500

    Have repetition time added now

---
+++ dicom_mr_metadata.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import pydicom
+import os
+import json
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Crontabs manager')
+    parser.add_argument("--dicom",dest='dicom_path',default=None,required=True, \
+                        help='Path to DICOM file to fetch metadata from')
+    parser.add_argument("--out",dest='outfile',default=None,required=False, \  #defaults to same dir and filename as dicom
+                        help='Path to save JSON object')
+    args = parser.parse_args()
+    return args
+
+def get_repetition_time(ds):
+    """
+
+    """
+    return ds["52009229"]['Value'][0]['00189112']['Value'][0]['00180080']['Value'][0]
+
+if __name__ == '__main__':
+    args = parse_args()
+    ds = pydicom.dcmread(args.dicom_path).to_json()
+    repetion_time = get_repetition_time(ds)
+
+
